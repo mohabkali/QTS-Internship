2bcfba3ba90293cf9a6981466259a99094f0df2d
mohabkali <91885981+mohabkali@users.noreply.github.com>
2025-05-31 21:25:21 +0300

    CalcFactorial

---
+++ week 2 exercise/CalculateFactorial/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Please enter a number:");
+string input = Console.ReadLine();
+if (int.tryParse(input, out num) && num > 0)
+{
+    Calculate.CalculateFactorial(num);
+}
+else {
+    Console.WriteLine("Please enter a valid numbber")
+}
+
+
+public class Calculate
+{
+    public static long CalculateFactorial(int number)
+    {
+        long factorial = 1;
+        if (number > 20)
+        {
+            Console.WriteLine("Number is too Large to Compute")
+            return -1;
+        }
+    }
+        else{
+            for (int i =1; i <= number; i+ ){
+                factorial *= i;
+                return factorial;
+            }
+        Console.WriteLine("Factorial is " + factorial);
+        }
+    }
+
--- week 2 exercise/StudentGradeCalc/obj/Debug/net9.0/StudentGradeCalc.AssemblyInfo.cs
+++ week 2 exercise/StudentGradeCalc/obj/Debug/net9.0/StudentGradeCalc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGradeCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+929f6e58518cda50ed497335b751713516582518")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGradeCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGradeCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
