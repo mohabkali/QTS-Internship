e8aadbb24593fe564a7f9151270ffb35021c81ff
mohabkali <91885981+mohabkali@users.noreply.github.com>
2025-06-03 20:05:41 +0300

    NextJs

--- week 2 exercise/WordReverser/WordReverser/obj/Debug/net9.0/WordReverser.AssemblyInfo.cs
+++ week 2 exercise/WordReverser/WordReverser/obj/Debug/net9.0/WordReverser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WordReverser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463a4f7c53f1b880ef74025d7908e10fd3ff2a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("WordReverser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WordReverser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
